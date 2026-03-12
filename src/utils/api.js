d8f08d1f52164950752eca58819791a271b27f9e
Nimi <oluwaloniagboluaje@gmail.com>
2026-03-11 20:52:25 -1000

    refactor: update axios instance baseURL to use environment variable for better configuration

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-// Create an axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -19,13 +18,11 @@ api.interceptors.request.use(
       if (now < parseInt(expiry)) {
         config.headers['Authorization'] = `Bearer ${token}`;
       } else {
-        // Token expired
         localStorage.removeItem('sr_token');
         localStorage.removeItem('sr_user');
         localStorage.removeItem('sr_expiry');
         delete config.headers['Authorization'];
         console.warn('Token expired. You have been logged out.');
-        // Optionally you can redirect the user here
       }
     }
     return config;
